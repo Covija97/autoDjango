54a92ed2f6eac2cbb5fd3a3cc101598ab4b43b02
Covija97 <48259969+Covija97@users.noreply.github.com>
2025-02-27 12:38:18 +0100

    upd 2.0

--- src/createdjango.bat
+++ src/createdjango.bat
@@ -1,51 +1,105 @@
 @echo off
+
 REM Check Python version
+echo --- Checking Python version...
+echo.
 python --version >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
-    echo Python is not installed. Please install it before continuing.
+    echo --- Python is not installed. Please install it before continuing.
+    pause
     exit /b
 )
 
 REM Check pip version
+echo --- Checking pip version...
+echo.
 pip --version >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
-    echo pip is not installed. Please install it before continuing.
+    echo --- pip is not installed. Please install it before continuing.
+    pause
     exit /b
 )
 
 REM Create virtual environment
+echo --- Creating virtual environment...
+echo.
 IF NOT EXIST myworld (
     python -m venv myworld
-    echo Virtual environment 'myworld' created.
+    echo --- Virtual environment 'myworld' created.
+    echo.
 ) ELSE (
-    echo The virtual environment 'myworld' already exists.
+    echo --- The virtual environment 'myworld' already exists.
+    echo.
 )
 
 REM Activate virtual environment
+echo --- Activating virtual environment...
+echo.
 call myworld\Scripts\activate.bat
 
 REM Upgrade pip
+echo --- Upgrading pip...
+echo.
 python -m pip install --upgrade pip
 
 REM Install Django
+echo.
+echo --- Installing Django...
+echo.
 pip install django
 
 REM Request Django project name
-set /p project_name=Enter the name of the new Django project: 
+echo.
+set invalid_names=django myworld Django /
+
+:request_project_name
+set /p project_name=--- Enter the name of the new Django project:
+echo.
+
+
+REM Check if project_name is empty
+if "%project_name%"=="" (
+    echo --- Project name cannot be empty.
+    echo.
+    goto request_project_name
+)
+
+REM Check if project_name is in invalid_names
+for %%i in (%invalid_names%) do (
+    if /i "%%i"=="%project_name%" (
+        echo --- Invalid project name. Please choose a different name.
+        echo.
+        goto request_project_name
+    )
+)
 
 REM Create Django project
+echo --- Creating Django project...
+echo.
 django-admin startproject %project_name%
 
 REM Verify project creation
+echo Verifying project creation...
+echo.
 IF EXIST %project_name% (
-    echo Django project '%project_name%' created successfully.
+    echo --- Django project '%project_name%' created successfully.
+    echo.
 ) ELSE (
-    echo Error creating the Django project.
+    echo --- Error creating the Django project.
+    pause
     exit /b
 )
 
 REM Create run script
-echo @echo off > run_%project_name%.bat
-echo call myworld\Scripts\activate.bat >> run_%project_name%.bat
-echo python manage.py runserver >> run_%project_name%.bat
-echo Run script 'run_%project_name%.bat' created to start the development server.
+echo --- Creating run script...
+echo.
+echo @echo off                                  > run_%project_name%.bat
+echo title Run server - %project_name%          >> run_%project_name%.bat
+echo call myworld\Scripts\activate.bat          >> run_%project_name%.bat
+echo python %project_name%\manage.py runserver  >> run_%project_name%.bat
+echo pause                                      >> run_%project_name%.bat
+
+REM Finish
+echo --- Django project setup completed successfully.
+echo.
+pause
